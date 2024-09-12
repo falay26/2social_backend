0f139b2bdecd2a28becfcb9c8614ad37f4f27868
Mehmet Serdar Falay <vodece@gmail.com>
2024-09-12 13:10:15 +0300

    barışfix4

--- server.js
+++ server.js
@@ -27,10 +27,8 @@ app.use(credentials);
 app.use(cors({ credentials: true, origin: true }));
 
 // built-in middleware to handle urlencoded form data
-app.use(express.urlencoded({ extended: false }));
-
-// built-in middleware for json
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 //middleware for cookies
 app.use(cookieParser());
