204f21e1d43aa2814a52e8c8b4cb07e6120f7600
Mehmet Serdar Falay <vodece@gmail.com>
2024-09-12 13:46:44 +0300

    barışfix9

--- helpers/postFormatter.js
+++ helpers/postFormatter.js
@@ -9,7 +9,7 @@ const postFormatter = (array, user) => {
       profilePhoto: item?.user[0]?.profile_picture,
       category: item?.category[0]?.name[user?.preferred_language],
       categoryIcon: item?.category[0]?.icon,
-      follow: user?.following?.includes(item?.user?._id),
+      follow: user?.following?.includes(item?.user[0]?._id),
       official: item?.user[0]?.premium,
       createdData: item?.created_at, //TODO: maybe format this further.
       likeCount: item?.likes?.length,
