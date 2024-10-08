981d13b95ecec7a81da201bc0b973056e98d76a7
Mehmet Serdar Falay <vodece@gmail.com>
2024-10-08 18:35:00 +0300

    otp passwrod changed

--- services/OtpService.js
+++ services/OtpService.js
@@ -6,7 +6,7 @@ const OtpService = async (phone, message) => {
     url: "https://api.netgsm.com.tr/sms/send/otp",
     headers: { "Content-Type": "application/xml" },
     data:
-      '<?xml version="1.0"?>\r\n<mainbody>\r\n   <header>\r\n       <usercode>8503039871</usercode>\r\n       <password>P1-1QVP4</password>\r\n       <msgheader>BLACKHOLE</msgheader>\r\n       <appkey></appkey> \r\n   </header>\r\n   <body>\r\n       <msg>\r\n           <![CDATA[' +
+      '<?xml version="1.0"?>\r\n<mainbody>\r\n   <header>\r\n       <usercode>8503039871</usercode>\r\n       <password>91A91DA</password>\r\n       <msgheader>BLACKHOLE</msgheader>\r\n       <appkey></appkey> \r\n   </header>\r\n   <body>\r\n       <msg>\r\n           <![CDATA[' +
       message +
       "]]>\r\n       </msg>\r\n       <no>" +
       phone +
